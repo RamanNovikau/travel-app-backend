0b069f3b416e56146761a9b123826591ea8949b8
Raman Novikau <demoikk@gmail.com>
2021-03-12 19:09:48 +0300

    feat: add get country by id

--- src/modules/countries/country.repository.js
+++ src/modules/countries/country.repository.js
@@ -1,11 +1,12 @@
 const Country = require('./country.schema');
+const { Types } = require('mongoose');
 
 const getAllByLang = async (lang) => {
     return Country.aggregate([{ $unwind: { path: '$localizations' } }, { $match: { "localizations.lang": lang } }]);
 };
 
-const getOneByLang = async (lang) => {
-    return Country.aggregate([{ $unwind: { path: '$localizations' } }, { $match: { "localizations.lang": lang } }]);
+const getOneByLang = async (id, lang) => {
+    return Country.aggregate([{ $match: { _id: Types.ObjectId(id) } }]);
 };
 
 module.exports = {
--- src/modules/countries/country.router.js
+++ src/modules/countries/country.router.js
@@ -8,4 +8,11 @@ router.get('/', async (request, response) => {
     response.send(data);
 });
 
+router.get('/:id', async (request, response) => {
+    const lang = request.query.lang || 'en';
+    const { id } = request.params;
+    const data = await countryService.getOne(id, lang);
+    response.send(data);
+});
+
 module.exports = router;
