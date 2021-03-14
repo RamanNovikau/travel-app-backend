4d111dc7f984e7673601f47fee6243465661ccf1
Raman Novikau <demoikk@gmail.com>
2021-03-14 13:47:44 +0300

    fox: change place router

--- src/modules/places/place.router.js
+++ src/modules/places/place.router.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = express.Router();
 const placeService = require('./place.service');
 
-router.get('/', async (request, response, next) => {
-    const lang = request.query.lang || 'en';
-    if (!request.query.country) {
+router.get('/', async (req, res, next) => {
+    const lang = req.query.lang || 'en';
+    if (!req.query.country) {
         const err = new Error('Required query params missing');
         err.status = 400;
         next(err);
     }
-    const countryId = request.query.country;
+    const countryId = req.query.country;
     const data = await placeService.getAll(countryId, lang);
-    response.send(data);
+    res.send(data);
 });
 
 module.exports = router;
