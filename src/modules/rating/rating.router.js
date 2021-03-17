dc896a359a760e13651f07bb5d11dee0aae804f8
Raman Novikau <demoikk@gmail.com>
2021-03-17 19:27:27 +0300

    fix: add user verify on publish rating

--- src/modules/rating/rating.router.js
+++ src/modules/rating/rating.router.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
+const dotenv = require('dotenv');
 const ratingService = require('./rating.service');
+const userService = require('../users/user.service');
 
 router.get('/', async (req, res, next) => {
   if (!req.query.place) {
@@ -18,17 +21,31 @@ router.post('/publish', async (req, res) => {
   try {
     console.log(req.body);
     const {
-      placeId, userId, date, comment, rating
+      placeId, userId, date, comment, rating, email, token
     } = req.body;
-    const publishRating = await ratingService.addRating({
-      placeId,
-      userId,
-      date,
-      comment,
-      rating
-    });
-    res.status(201).json({
-      publishRating
+
+    const user = await userService.getOneByEmail({ email });
+
+    if (!user) {
+      return res.status(401).json({ message: 'unauthorized' });
+    }
+
+    const verify = jwt.verify(token, process.env.JWT_SECRET, async (err, verifiedJwt) => {
+      if (err) {
+        return res.status(401).json({ message: 'unauthorized' });
+      }
+
+      const publishRating = await ratingService.addRating({
+        placeId,
+        userId,
+        date,
+        comment,
+        rating
+      });
+
+      res.status(201).json({
+        publishRating
+      });
     });
   }
   catch (e) {
